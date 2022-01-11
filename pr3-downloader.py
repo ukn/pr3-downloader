489640273eb9ff998c43074d716b82b3ffd23c16
Maciej Dworakowski <ukn@users.noreply.github.com>
2022-01-11 20:04:47 +0000

    Show progress of downloads

--- pr3-downloader.py
+++ pr3-downloader.py
@@ -2,11 +2,12 @@
 
 import os
 import re
-from sys import argv
+from sys import argv, exit, stderr
 import json
-import urllib
 from pathlib import Path
+from functools import reduce
 from multiprocessing.pool import ThreadPool
+from time import sleep
 from lxml import html
 from lxml import etree as et
 import requests
@@ -16,10 +17,14 @@ if "PR3_DEBUG" in os.environ:
     DEBUG = bool(os.environ["PR3_DEBUG"])
     print("DEBUG - verbose logging and not downloading, doing first element only")
 
+THREADPOOL = 1
+if "THREADPOOL" in os.environ and int(os.environ["THREADPOOL"]) > 0:
+    THREADPOOL = int(os.environ["THREADPOOL"])
+
 PR3_BASE_URL = "https://www.polskieradio.pl"
 
 
-def getContent(base_url, page_number=1):
+def getContent(base_url, start_page=1, end_page=None):
     ses = requests.Session()
     base_req = ses.get(base_url)
     tree = html.fromstring(base_req.text)
@@ -27,27 +32,31 @@ def getContent(base_url, page_number=1):
         pages = tree.xpath("//div[starts-with(@id,'ctl00_pager')]")[0]
         last_page_html = pages.xpath("./ul/li")[-1:][0]
         last_page = last_page_html.xpath("./a//text()")[0].strip()
+        if end_page == None:
+            end_page = int(last_page)
         tab_params = last_page_html.xpath("./a//@onclick")
-        print("Pages:", last_page)
-
-        for page_no in range(int(page_number), int(last_page) + 1):
+        print(
+            f"Pages to process: {start_page} - {end_page} out of {last_page}")
+        for page_no in range(int(start_page), int(end_page)+1):
+            global STATE
+            STATE = {}
             if len(tab_params) == 1:
                 articles = get_arts_from_tabs_content(
                     ses, tab_params[0], page_no)
             else:
                 articles = get_arts_from_pages(ses, base_url, page_no)
-
-            print("  Page", page_no, "of", last_page,
-                  "(", len(articles), "files )")
+            print("  Page", page_no, "(", len(articles), "files )")
             files_2_download = parse_articles(ses, articles)
-            ThreadPool(1).map(download, files_2_download)
+
+            ThreadPool(THREADPOOL).map(download, files_2_download)
             if DEBUG:
                 break
     except IndexError:
-        articleSoundsList = tree.xpath("//div[@id = 'articleSoundsList']//ul/li")
+        articleSoundsList = tree.xpath(
+            "//div[@id = 'articleSoundsList']//ul/li")
         print(len(articleSoundsList), "files to download")
         files_2_download = parse_articles(ses, articleSoundsList)
-        ThreadPool(1).map(download, files_2_download)
+        ThreadPool(THREADPOOL).map(download, files_2_download)
 
 
 def parse_articles(ses, articles):
@@ -59,11 +68,10 @@ def parse_articles(ses, articles):
             article_body = ses.get(article)
             article_html = html.fromstring(article_body.text)
         else:
-            # print("ARTICEL",html.tostring(article))
             article_title_pre = article.xpath(
                 "./div/a[@class = 'pr-media-play']/@data-media")[0]
             article_title = json.loads(article_title_pre)['desc']
-            #print(article_title)
+            # print(article_title)
             article_html = article
         try:
             mp3_mess = article_html.xpath(
@@ -81,7 +89,7 @@ def parse_articles(ses, articles):
                       article_title, "so skipping")
                 continue
         files_for_download.append(
-            {"url": file_full_url, "file": article_title + ".mp3"})
+            {"url": file_full_url, "file": article_title.lower() + ".mp3"})
         if DEBUG:
             return files_for_download
     return files_for_download
@@ -128,37 +136,67 @@ def get_arts_from_tabs_content(ses, tab_options, page_number):
     return articles_url
 
 
-def download(pr3_object):
-    article_file_name = urllib.parse.unquote(pr3_object["file"]).replace("/", "-")
-    article_file = Path(article_file_name)
-    if article_file.is_file():
-        print("   ", article_file_name, "exists so skipping")
+def download(object):
+    columns = os.get_terminal_size().columns
+    global STATE
+    CLR = "\x1B[0K"
+    filename = f'{object["file"]}'
+    if Path(filename).is_file():
+        print(f"    {filename} exists so skipping")
         return
-    print("    Downloading " + article_file_name, end='')
     if DEBUG:
-        print(" DEBUG: " + pr3_object["url"])
+        print("[DEBUG] " + object["url"])
         return
-    else:
-        print("")
-    r = requests.get(pr3_object["url"], allow_redirects=True)
-    open(article_file_name, 'wb').write(r.content)
-    return
+    try:
+        r = requests.get(object["url"], allow_redirects=True, stream=True)
+        # Estimates the number of bar updates
+        block_size = 1024 * 1024
+        file_size = int(r.headers.get('Content-Length', None))
+        # print(file_size)
+        with open(filename + ".tmp", 'wb') as f:
+            for i, chunk in enumerate(r.iter_content(chunk_size=1 * block_size)):
+                if r.raise_for_status() or r.status_code != 200:
+                    raise Exception("Downloading error!")
+                f.write(chunk)
+                STATE[filename] = f'{int(((i*len(chunk))/file_size)*100)}%'
+                # display progress dynamically only if fits the terminal
+                state_length = reduce(
+                    lambda acc, file: acc + len(file) + len(STATE[file]), STATE, 10*THREADPOOL)
+                if columns > state_length:
+                    if (i % 4 == 0):
+                        print(f"{STATE}{CLR}", end="\r")
+        STATE[filename] = '100%'
+        os.rename(filename + ".tmp", filename)
+    except Exception as e:
+        print(f'Downloading stopped {filename}: {e}', file=stderr)
+        if Path(filename).is_file():
+            os.unlink(filename)
+        return filename
+    print(f"    {filename} downloaded{CLR}")
+    STATE.pop(filename)
+    return filename
 
 
 def printHelp():
     print("Zaciagacz audycji PR3\n\n" +
-          argv[0] + " link_do_audycji [numer_strony_poczatkowej=1]\n\n"
-          "na przyklad tak wyglada sciaganie audycji Aksamit od strony 5:\n"
+          argv[0] + " link_do_audycji [pierwsza_strona=1] [ostatnio_strona=10]\n\n"
+          "na przyklad tak wyglada sciaganie audycji Aksamit od strony 5 do ostatniej:\n"
           + argv[0] + " https://www.polskieradio.pl/9/5360 5")
 
 
 def main():
-    if len(argv) == 2:
-        getContent(argv[1])
-    elif len(argv) == 3:
-        getContent(argv[1], argv[2])
-    else:
-        printHelp()
+    try:
+        if len(argv) == 2:
+            getContent(argv[1])
+        elif len(argv) == 3:
+            getContent(argv[1], argv[2])
+        elif len(argv) == 4:
+            getContent(argv[1], argv[2], argv[3])
+        else:
+            printHelp()
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        exit()
 
 
 main()
